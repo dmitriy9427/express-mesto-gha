1730f69304779db43cbbb2558a352d6643cf963d
Dmitryi Ryabov <dima.poltergeist.2015@yandex.ru>
2022-11-09 11:12:29 +0300

    обновил проект

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const routesUsers = require('./routes/users');
-const routesCards = require('./routes/cards');
-const NotFound = require('./errors/NotFound');
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { registerValid, loginValid } = require('./middlewares/joi');
+const express = require("express");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const routesUsers = require("./routes/users");
+const routesCards = require("./routes/cards");
+const NotFound = require("./errors/NotFound");
+const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
+const { registerValid, loginValid } = require("./middlewares/joi");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3000 } = process.env;
 
@@ -15,32 +16,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
 // логин
-app.post('/signin', loginValid, login);
+app.post("/signin", loginValid, login);
 
 // регистрация
-app.post('/signup', registerValid, createUser);
+app.post("/signup", registerValid, createUser);
 
 app.use(auth);
 
-app.use(routesUsers);
-app.use(routesCards);
+app.use("/users", routesUsers);
+app.post("/posts", routesCards);
 
 app.use(() => {
-  throw new NotFound('Запрашиваемый ресурс не найден');
+  throw new NotFound("Запрашиваемый ресурс не найден");
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
 // централизованный обработчик ошибок
 app.use((err, req, res, next) => {
   const status = err.statusCode || 500;
-  const message = status === 500 ? 'На сервере произошла ошибка' : err.message;
+  const message = status === 500 ? "На сервере произошла ошибка" : err.message;
   res.status(status).send({ message });
   next();
 });
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
 });
 
---
+++ middlewares/cors.js
@@ -0,0 +1,31 @@
+const allowedCors = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "http://domainname.ryabov1994.nomoredomains.icu",
+  "http://domainname.ryabovdima.nomoredomains.icu",
+  "https://domainname.ryabov1994.nomoredomains.icu",
+  "https://domainname.ryabovdima.nomoredomains.icu",
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers["access-control-request-headers"];
+  console.log(req);
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    res.send();
+    return;
+  }
+  next();
+};
+
+module.exports = cors;
